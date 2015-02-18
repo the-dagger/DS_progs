9fe5da48750f39dd28f0fe69dc8518a548639604
dagger <manasbagula@gmail.com>
2015-02-18 16:23:47 +0530

    Sum of digits

---
+++ sum_of_digits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int sum(int);
+int temp=0,sum1=0;
+main()
+{int a;int i=0;
+cout<<"Enter the number\n";
+cin>>a;
+cout<<"The sum of the digits is "<<sum(a);
+}
+int sum(int b)
+{
+    if(b!=0)
+   {temp=b%10;
+    sum1+=temp;
+    b=b/10;
+    sum(b);}
+    else
+    return sum1;
+}
